d5c75a9c4d1dee8e80abaa5e25ca32a4bb171d49
Isaac Royle <isaac.royle@student.manchester.ac.uk>
2025-06-25 18:18:16 +0100

    Add raylib sample files

---
+++ src/engine.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include "common.h"
+#include "imgio.h"
+#include "list.h"
+#include "object.h"
+#include "transform.h"
+
+void RenderObject(Camera *camera, Object* object);
+void DepthVisualisation(Camera *cam, char* fileName);
+Pixel PixelColour(Object* object, float2 texture, float3 normal);
+
+int main(int argc, char *argv[]) {
+    // srand(time(NULL));
+    Camera* cam = CreateCamera(1080, 720, 90, 1000);
+
+    printf("Loading object files...\n");
+
+    Object* cube = LoadObjFile("assets/cube.obj");
+    cube->transform = (Transform){0, 0, 3, .5, -.5, 0};
+    cube->texture = ReadBMP("assets/CubeBoundsTexture.bmp");
+
+    Object* monkey = LoadObjFile("assets/monkey.obj");
+    monkey->transform = (Transform){0, 0, 2, .3, PI + .3, 0};
+    
+    Object* dragon = LoadObjFile("assets/dragon.obj");
+    dragon->transform = (Transform){0, 0, .6, 0, PI / 2, 0};
+
+    printf("Loaded object files!\n");
+
+    printf("Rendering scene...\n");
+    RenderObject(cam, cube);
+
+    printf("Saving rendered output...\n");
+    int ret = SaveBMP(cam->target, "output/out.bmp");
+    if (ret == -1) printf("FAILED TO SAVE RENDER\n");
+
+    printf("Cleaning up...\n");
+    DestroyCamera(cam);
+    FreeObject(cube);
+    FreeObject(monkey);
+    FreeObject(dragon);
+
+    printf("Done!\n");
+    return 0;
+}
+
+void RenderObject(Camera* camera, Object* object) {
+    for (int i = 0; i < object->triCount; i++) {
+        float3 a = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[0]));
+        float3 b = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[1]));
+        float3 c = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[2]));
+
+        if (a.z <= 0 || b.z <= 0 || c.z <= 0) continue;
+
+        float minX = clamp(fminf(fminf(a.x, b.x), c.x), 0, camera->target->width);
+        float minY = clamp(fminf(fminf(a.y, b.y), c.y), 0, camera->target->height);
+        float maxX = clamp(fmaxf(fmaxf(a.x, b.x), c.x), 0, camera->target->width);
+        float maxY = clamp(fmaxf(fmaxf(a.y, b.y), c.y), 0, camera->target->height);
+        
+        for (int x = floor(minX); x < ceil(maxX); x++)
+        for (int y = floor(minY); y < ceil(maxY); y++) {
+            float3 weights;
+            
+            if (!PointInTriangle((float2){(float)x, (float)y}, IgnoreZ(a), IgnoreZ(b), IgnoreZ(c), &weights)) continue;
+            
+            float depth = 1/Dot3(Inverse3((float3){ a.z, b.z, c.z }), weights);
+            if (camera->depth[x][y] < depth) continue;
+            
+            float2 texture = ZERO2;
+            texture = Add2(texture, Scale2(object->tris[i].texture[0], weights.x / a.z));
+            texture = Add2(texture, Scale2(object->tris[i].texture[1], weights.y / b.z));
+            texture = Add2(texture, Scale2(object->tris[i].texture[2], weights.z / c.z));
+            texture = Scale2(texture, depth);
+            
+            float3 normal = ZERO3;
+            normal = Add3(normal, Scale3(object->tris[i].normal[0], weights.x / a.z));
+            normal = Add3(normal, Scale3(object->tris[i].normal[1], weights.y / b.z));
+            normal = Add3(normal, Scale3(object->tris[i].normal[2], weights.z / c.z));
+            normal = Scale3(normal, depth);
+            normal = Normalise(normal);
+            normal = Rotate3(normal, object->transform.rot);
+            
+            camera->target->image[x][y] = PixelColour(object, texture, normal);
+            camera->depth[x][y] = depth;
+        }
+    }
+}
+
+void DepthVisualisation(Camera *cam, char* fileName) {
+    float min = cam->maxDistance, max = 0;
+    for (int x = 0; x < cam->target->width; x++)
+    for (int y = 0; y < cam->target->height; y++) {
+        if (cam->depth[x][y] < min) min = cam->depth[x][y];
+        if (cam->depth[x][y] > max && cam->depth[x][y] < cam->maxDistance) max = cam->depth[x][y];
+    }
+    Texture* texture = CreateImage(cam->target->width, cam->target->height);
+    for (int x = 0; x < cam->target->width; x++)
+    for (int y = 0; y < cam->target->height; y++) {
+        float col;
+        if (cam->depth[x][y] == cam->maxDistance) col = 0.2;
+        else col = (cam->depth[x][y] - min) / (max - min);
+        texture->image[x][y] = (Pixel){ col * 255, col * 255, col * 255 };
+    }
+    SaveBMP(texture, fileName);
+    FreeImage(texture);
+}
+
+Pixel PixelColour(Object* object, float2 texture, float3 normal) {
+    float lightIntensity = (Dot3(normal, (float3){0, 1, 0}) + 1) * 0.5;
+    lightIntensity = lerp(0, 1, lightIntensity);
+    
+    Pixel colour;
+    if (object->texture == NULL) 
+        colour = object->colour;
+    else {
+        float u = 1-texture.x; // scale by texture scale in future
+        float v = texture.y; // * object->textureScale ?
+
+        int x = (int)roundf((u - floorf(u)) * (object->texture->width-1));
+        int y = (int)roundf((v - floorf(v)) * (object->texture->height-1));
+
+        x = clamp(x, 0, object->texture->width-1);
+        y = clamp(y, 0, object->texture->height-1);
+
+        colour = object->texture->image[x][y];
+    }
+
+    return Vec3ToColour(Scale3(ColourToVec3(colour), lightIntensity));
+}
+
+/*
+float u = texCoord.x * TextureScale;
+float v = texCoord.y * TextureScale
+
+// Calculate indices of nearest texel to sample point
+int x = (int)((u - floorf(u)) * (object->texture->width-1));
+int y = (int)((v - floorf(v)) * (object->texture->height-1));
+
+colour image[x, y];
+*/
--- src/main.c
+++ src/main.c
@@ -1,143 +1,92 @@
-#include <stdio.h>
 #include <math.h>
-#include <time.h>
 
-#include "common.h"
-#include "imgio.h"
-#include "list.h"
-#include "object.h"
-#include "transform.h"
+#include "raylib.h"
 
-void RenderObject(Camera *camera, Object* object);
-void DepthVisualisation(Camera *cam, char* fileName);
-Pixel PixelColour(Object* object, float2 texture, float3 normal);
+typedef struct float3 {
+    float x;
+    float y;
+    float z;
+} float3;
 
-int main(int argc, char *argv[]) {
-    // srand(time(NULL));
-    Camera* cam = CreateCamera(1080, 720, 90, 1000);
+#define WIDTH 1280
+#define HEIGHT 720
 
-    printf("Loading object files...\n");
+void Flatten(float3** TD, Color* OD);
+void Clear(float3** A);
 
-    Object* cube = LoadObjFile("assets/cube.obj");
-    cube->transform = (Transform){0, 0, 3, .5, -.5, 0};
-    cube->texture = ReadBMP("assets/CubeBoundsTexture.bmp");
+int main()
+{
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+	InitWindow(WIDTH, HEIGHT, "Test");
 
-    Object* monkey = LoadObjFile("assets/monkey.obj");
-    monkey->transform = (Transform){0, 0, 2, .3, PI + .3, 0};
-    
-    Object* dragon = LoadObjFile("assets/dragon.obj");
-    dragon->transform = (Transform){0, 0, .6, 0, PI / 2, 0};
+    Texture texture = LoadTextureFromImage(GenImageColor(WIDTH, HEIGHT, BLACK));
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
 
-    printf("Loaded object files!\n");
+    Color *texColBuffer = MemAlloc(sizeof(Color) * WIDTH * HEIGHT);
 
-    printf("Rendering scene...\n");
-    RenderObject(cam, cube);
+	float3 **target = MemAlloc(sizeof(float3*) * WIDTH);
+    for (int x = 0; x < WIDTH; x++)
+        target[x] = MemAlloc(sizeof(float3) * HEIGHT);
+    Clear(target);
 
-    printf("Saving rendered output...\n");
-    int ret = SaveBMP(cam->target, "output/out.bmp");
-    if (ret == -1) printf("FAILED TO SAVE RENDER\n");
+    int pos[2] = {100, 100};
 
-    printf("Cleaning up...\n");
-    DestroyCamera(cam);
-    FreeObject(cube);
-    FreeObject(monkey);
-    FreeObject(dragon);
+	while (!WindowShouldClose())
+	{
 
-    printf("Done!\n");
-    return 0;
-}
+        // Update Scene
+        if (IsKeyDown(KEY_LEFT)) pos[0]--;
+        if (IsKeyDown(KEY_RIGHT)) pos[0]++;
+        if (IsKeyDown(KEY_UP)) pos[1]++;
+        if (IsKeyDown(KEY_DOWN)) pos[1]--;
 
-void RenderObject(Camera* camera, Object* object) {
-    for (int i = 0; i < object->triCount; i++) {
-        float3 a = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[0]));
-        float3 b = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[1]));
-        float3 c = WorldToScreen(camera, LocalToWorld(object->transform, object->tris[i].vertex[2]));
-
-        if (a.z <= 0 || b.z <= 0 || c.z <= 0) continue;
-
-        float minX = clamp(fminf(fminf(a.x, b.x), c.x), 0, camera->target->width);
-        float minY = clamp(fminf(fminf(a.y, b.y), c.y), 0, camera->target->height);
-        float maxX = clamp(fmaxf(fmaxf(a.x, b.x), c.x), 0, camera->target->width);
-        float maxY = clamp(fmaxf(fmaxf(a.y, b.y), c.y), 0, camera->target->height);
-        
-        for (int x = floor(minX); x < ceil(maxX); x++)
-        for (int y = floor(minY); y < ceil(maxY); y++) {
-            float3 weights;
-            
-            if (!PointInTriangle((float2){(float)x, (float)y}, IgnoreZ(a), IgnoreZ(b), IgnoreZ(c), &weights)) continue;
-            
-            float depth = 1/Dot3(Inverse3((float3){ a.z, b.z, c.z }), weights);
-            if (camera->depth[x][y] < depth) continue;
-            
-            float2 texture = ZERO2;
-            texture = Add2(texture, Scale2(object->tris[i].texture[0], weights.x / a.z));
-            texture = Add2(texture, Scale2(object->tris[i].texture[1], weights.y / b.z));
-            texture = Add2(texture, Scale2(object->tris[i].texture[2], weights.z / c.z));
-            texture = Scale2(texture, depth);
-            
-            float3 normal = ZERO3;
-            normal = Add3(normal, Scale3(object->tris[i].normal[0], weights.x / a.z));
-            normal = Add3(normal, Scale3(object->tris[i].normal[1], weights.y / b.z));
-            normal = Add3(normal, Scale3(object->tris[i].normal[2], weights.z / c.z));
-            normal = Scale3(normal, depth);
-            normal = Normalise(normal);
-            normal = Rotate3(normal, object->transform.rot);
-            
-            camera->target->image[x][y] = PixelColour(object, texture, normal);
-            camera->depth[x][y] = depth;
+        // Render Scene to target
+        for (int x = 0; x < WIDTH; x++)
+        for (int y = 0; y < HEIGHT; y++) {
+            target[x][y] = (float3){(float)x/WIDTH, (float)y/HEIGHT, 0};
         }
+        target[pos[0]][pos[1]] = (float3){1, 1, 1};
+
+        Flatten(target, texColBuffer);
+        UpdateTexture(texture, texColBuffer);
+        Clear(target);
+
+        Rectangle src = (Rectangle){0, HEIGHT, WIDTH, -HEIGHT};
+        Rectangle dest = (Rectangle){0, 0, GetScreenWidth(), GetScreenHeight()};
+        Vector2 origin = (Vector2){0, 0};
+
+        // Draw
+        BeginDrawing();
+        DrawTexturePro(texture, src, dest, origin, 0.0f, WHITE);
+        DrawFPS(10, 10);
+		EndDrawing();
+	}
+
+    for (int x = 0; x < WIDTH; x++)
+        MemFree(target[x]);
+    MemFree(target);
+
+    MemFree(texColBuffer);
+    UnloadTexture(texture);
+
+	CloseWindow();
+	return 0;
+}
+
+void Flatten(float3** TD, Color* OD) {
+    for (int x = 0; x < WIDTH; x++)
+    for (int y = 0; y < HEIGHT; y++) {
+        OD[WIDTH*y + x] = (Color){
+            roundf(TD[x][y].x * 255),
+            roundf(TD[x][y].y * 255),
+            roundf(TD[x][y].z * 255),
+            255
+        };
     }
 }
 
-void DepthVisualisation(Camera *cam, char* fileName) {
-    float min = cam->maxDistance, max = 0;
-    for (int x = 0; x < cam->target->width; x++)
-    for (int y = 0; y < cam->target->height; y++) {
-        if (cam->depth[x][y] < min) min = cam->depth[x][y];
-        if (cam->depth[x][y] > max && cam->depth[x][y] < cam->maxDistance) max = cam->depth[x][y];
-    }
-    Texture* texture = CreateImage(cam->target->width, cam->target->height);
-    for (int x = 0; x < cam->target->width; x++)
-    for (int y = 0; y < cam->target->height; y++) {
-        float col;
-        if (cam->depth[x][y] == cam->maxDistance) col = 0.2;
-        else col = (cam->depth[x][y] - min) / (max - min);
-        texture->image[x][y] = (Pixel){ col * 255, col * 255, col * 255 };
-    }
-    SaveBMP(texture, fileName);
-    FreeImage(texture);
+void Clear(float3** A) {
+    for (int x = 0; x < WIDTH; x++)
+    for (int y = 0; y < HEIGHT; y++)
+        A[x][y] = (float3){0, 0, 0};    
 }
-
-Pixel PixelColour(Object* object, float2 texture, float3 normal) {
-    float lightIntensity = (Dot3(normal, (float3){0, 1, 0}) + 1) * 0.5;
-    lightIntensity = lerp(0, 1, lightIntensity);
-    
-    Pixel colour;
-    if (object->texture == NULL) 
-        colour = object->colour;
-    else {
-        float u = 1-texture.x; // scale by texture scale in future
-        float v = texture.y; // * object->textureScale ?
-
-        int x = (int)roundf((u - floorf(u)) * (object->texture->width-1));
-        int y = (int)roundf((v - floorf(v)) * (object->texture->height-1));
-
-        x = clamp(x, 0, object->texture->width-1);
-        y = clamp(y, 0, object->texture->height-1);
-
-        colour = object->texture->image[x][y];
-    }
-
-    return Vec3ToColour(Scale3(ColourToVec3(colour), lightIntensity));
-}
-
-/*
-float u = texCoord.x * TextureScale;
-float v = texCoord.y * TextureScale
-
-// Calculate indices of nearest texel to sample point
-int x = (int)((u - floorf(u)) * (object->texture->width-1));
-int y = (int)((v - floorf(v)) * (object->texture->height-1));
-
-colour image[x, y];
-*/
